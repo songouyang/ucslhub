a897d35f07c148e44b437d1d85bfb3a86b22d846
Mohit Sharma <mohitsharma44@gmail.com>
2019-01-28 18:05:42 -0500

    upated nbgrader_config

--- images/student_notebook/nbgrader_config.py
+++ images/student_notebook/nbgrader_config.py
@@ -1,3 +1,10 @@
+import os
+import glob
+
 c = get_config()
-c.Exchange.root = '/home/jovyan/work/assignments/'
+
+user_dir = [x for x in glob.glob('/home/jovyan/**', recursive=True) if os.environ['username'] in x][0]
+
+c.CourseDirectory.root = os.path.join(user_dir, 'work')
+c.Exchange.root = os.path.join(user_dir, 'work', 'assignments')
 c.Exchange.course_id = 'UCSL-bootcamp'
