207e0e64039b968a2e4fdc58a627dafb125e4016
Mohit Sharma <mohitsharma44@gmail.com>
2019-01-28 18:05:23 -0500

    using master nbgrader version for its utils.get_username and uils.get_osusername differences

--- images/ta_notebook/nbgrader_config.py
+++ images/ta_notebook/nbgrader_config.py
@@ -1,3 +1,9 @@
+import os
+import glob
+
 c = get_config()
-c.Exchange.root = '/home/jovyan/work/assignments/'
+
+ta_dir = [x for x in glob.glob('/home/jovyan/**', recursive=True) if os.environ['ta'] in x][0]
+c.CourseDirectory.root = os.path.join(ta_dir, 'work')
+c.Exchange.root = os.path.join(ta_dir, 'work', 'assignments')
 c.Exchange.course_id = 'UCSL-bootcamp'
