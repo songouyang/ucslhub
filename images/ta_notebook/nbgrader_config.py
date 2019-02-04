c5c4c3ece97f1a612bacde90e9b6fbc6961f2bd8
Mohit Sharma <mohitsharma44@gmail.com>
2019-02-04 16:28:39 -0500

    using /srv/nbgrader

--- images/ta_notebook/nbgrader_config.py
+++ images/ta_notebook/nbgrader_config.py
@@ -5,5 +5,5 @@ c = get_config()
 
 ta_dir = [x for x in glob.glob('/home/jovyan/**', recursive=True) if os.environ['ta'] in x][0]
 c.CourseDirectory.root = os.path.join(ta_dir, 'work')
-c.Exchange.root = os.path.join(ta_dir, 'work', 'assignments')
+#c.Exchange.root = os.path.join(ta_dir, 'work', 'assignments')
 c.Exchange.course_id = 'UCSL-bootcamp'
